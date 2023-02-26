dbdca6919c675a9cae931e5b1f869c945ea6d3b5
Elizaveta_Olenchenko <elizaveta_olenchenko@epam.com>
2023-02-27 00:31:48 +0100

    task-3

---
+++ product-service-pillow-typescript/src/mock/pillows.ts
@@ -0,0 +1,85 @@
+import { Pillow } from "src/model/pillow";
+
+export const PILLOW: Pillow[] = [
+    {
+        "id": "1",
+        "pillowName": "pillow 1",
+        "matherials": "Cotton",
+        "filler": "Down",
+        "height": 40, 
+        "width": 40,
+        "price": "100 eur"
+    },
+    {
+        "id": "2",
+        "pillowName": "pillow 2",
+        "matherials": "Synthetic fibers",
+        "filler": "Polyester Fiberfill",
+        "height": 50, 
+        "width": 50,
+        "price": "50 eur"
+    },
+    {
+        "id": "3",
+        "pillowName": "pillow 3",
+        "matherials": "Velvet",
+        "filler": "Feather Pillow",
+        "height": 50, 
+        "width": 70,
+        "price": "350 eur"
+    },
+    {
+        "id": "4",
+        "pillowName": "pillow 4",
+        "matherials": "Synthetic fibers",
+        "filler": "Memory Foam",
+        "height": 50, 
+        "width": 70,
+        "price": "250 eur"
+    },
+    {
+        "id": "5",
+        "pillowName": "pillow 5",
+        "matherials": "Synthetic fibers",
+        "filler": "Buckwheat Hull",
+        "height": 70, 
+        "width": 70,
+        "price": "450 eur"
+    },
+    {
+        "id": "6",
+        "pillowName": "pillow 6",
+        "matherials": "Cotton",
+        "filler": "Kapok",
+        "height": 70, 
+        "width": 70,
+        "price": "300 eur"
+    },
+    {
+        "id": "7",
+        "pillowName": "pillow 7",
+        "matherials": "Cotton",
+        "filler": "Wool",
+        "height": 60, 
+        "width": 40,
+        "price": "500 eur"
+    },
+    {
+        "id": "8",
+        "pillowName": "pillow 8",
+        "matherials": "Synthetic fibers",
+        "filler": "Latex",
+        "height": 50, 
+        "width": 70,
+        "price": "370 eur"
+    },
+    {
+        "id": "9",
+        "pillowName": "pillow 9",
+        "matherials": "Synthetic fibers",
+        "filler": "Microbead",
+        "height": 50, 
+        "width": 70,
+        "price": "210 eur"
+    }
+]

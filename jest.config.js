ed3085dc4096c1beaa74b6a20804e511db3fe2db
Elizaveta_Olenchenko <elizaveta_olenchenko@epam.com>
2023-03-27 22:51:22 +0300

    delete old src

--- jest.config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    preset: 'ts-jest',
-    testEnvironment: 'node',
-    moduleNameMapper: {
-      '^@libs/api-gateway$': '<rootDir>/src/libs/api-gateway.ts',
-    },
-};
--- src/model/pillow.ts
+++
@@ -1,9 +0,0 @@
-export interface Pillow {
-    id: string
-    pillowName: string
-    matherials: string
-    filler: string
-    height: number
-    width: number
-    price: number
-}

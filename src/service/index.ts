f3de0acf3cd941f3854888e331fdd268b152c911
Elizaveta_Olenchenko <elizaveta_olenchenko@epam.com>
2023-04-10 00:31:18 +0300

    delete src

--- src/service/index.ts
+++
@@ -1,5 +0,0 @@
-import dynamoDBClient from "../model/database";
-import PillowServise from "./pillowServise"
-
-const pillowServerice = new PillowServise(dynamoDBClient());
-export default pillowServerice;
